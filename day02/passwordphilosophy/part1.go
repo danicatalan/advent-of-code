e1e44e14ca029949ae9b58e3747da53aed435301
Dani Catalan <danicatalan@hotmail.com>
2020-12-02 18:33:34 +0100

    day02 improve readability

--- day02/passwordphilosophy/part1.go
+++ day02/passwordphilosophy/part1.go
@@ -1,9 +1,7 @@
 package passwordphilosophy
 
 import (
-	"log"
-	"regexp"
-	"strconv"
+	"fmt"
 	"strings"
 )
 
@@ -11,20 +9,12 @@ import (
 // The password policy indicates the lowest and highest number of times
 // a given letter must appear for the password to be valid.
 func Part1(input string) bool {
-	regex := regexp.MustCompile(`-| |: `)
-	p := regex.Split(input, -1)
+	var min, max int
+	var letter byte
+	var password string
+	fmt.Sscanf(input, "%d-%d %c: %v", &min, &max, &letter, &password)
 
-	c := strings.Count(p[3], p[2])
-
-	min, err := strconv.Atoi(p[0])
-	if err != nil {
-		log.Fatal(err)
-	}
-	max, err := strconv.Atoi(p[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	c := strings.Count(password, string(letter))
 	if c >= min && c <= max {
 		return true
 	}
--- day02/passwordphilosophy/part2.go
+++ day02/passwordphilosophy/part2.go
@@ -1,9 +1,7 @@
 package passwordphilosophy
 
 import (
-	"log"
-	"regexp"
-	"strconv"
+	"fmt"
 )
 
 // Part2 Each policy actually describes two positions in the password,
@@ -11,22 +9,12 @@ import (
 // Exactly one of these positions must contain the given letter.
 // Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
 func Part2(input string) bool {
-	regex := regexp.MustCompile(`-| |: `)
-	p := regex.Split(input, -1)
+	var pos1, pos2 int
+	var letter byte
+	var password string
+	fmt.Sscanf(input, "%d-%d %c: %v", &pos1, &pos2, &letter, &password)
 
-	p1, err := strconv.Atoi(p[0])
-	if err != nil {
-		log.Fatal(err)
-	}
-	p2, err := strconv.Atoi(p[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if p[2] == string(p[3][p1-1]) && p[2] != string(p[3][p2-1]) {
-		return true
-	}
-	if p[2] != string(p[3][p1-1]) && p[2] == string(p[3][p2-1]) {
+	if (letter == password[pos1-1] || letter == password[pos2-1]) && (password[pos1-1] != password[pos2-1]) {
 		return true
 	}
 	return false
